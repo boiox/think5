e65c9329d7ac3fc131c67aad0ddbc1eed2678061
company_boiox <company_boiox@qq.com>
2016-10-11 14:51:14 +0800

    添加微信第三方拉取用户信息（未完成版）

--- public/index.php
+++ public/index.php
@@ -14,5 +14,14 @@
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../app/');
 
+// 主机协议
+define('SITE_PROTOCOL', isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == '443' ? 'https://' : 'http://');
+
+// 主机名
+define('SITE_URL', (isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : ''));
+
+// 来源
+define('HTTP_REFERER', isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : '');
+
 // 加载框架引导文件
 require __DIR__ . '/../core/start.php';
